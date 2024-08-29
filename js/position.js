cabcdc6cf12dc444201110f4d4ad1eb5ac1d92be
ChiaChun1997 <alexchow297@gmail.com>
2024-08-29 16:02:12 +0800

    backToTop按鈕完成，按鈕動畫效果待研究

--- js/position.js
+++ js/position.js
@@ -18,12 +18,38 @@ function scrollToPosition(link) {
   });
 }
 
+// 回到頂部 function
+function scrollToTop(link) {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
 const aboutLink = document.querySelector("#aboutLink");
 const portfolioLink = document.querySelector("#portfolioLink");
 const contactLink = document.querySelector("#contactLink");
 const contactBtnLink = document.querySelector("#contactBtnLink");
+const logoLink = document.querySelector("#logoLink");
+const backToTopBtn = document.querySelector("#backToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopBtn.classList.add("show");
+    backToTopBtn.classList.remove("hide");
+  } else {
+    backToTopBtn.classList.add("hide");
+    backToTopBtn.classList.remove("show");
+  }
+});
 
 scrollToPosition(aboutLink);
 scrollToPosition(portfolioLink);
 scrollToPosition(contactLink);
 scrollToPosition(contactBtnLink);
+scrollToTop(logoLink);
+scrollToTop(backToTopBtn);
